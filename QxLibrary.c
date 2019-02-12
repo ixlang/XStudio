534bdfdcb19cbbd3e360f92df8d9c8ac947515aa
ixlang <dxmer@qq.com>
2019-02-13 01:34:14 +0800

    更新版本
    优化调试时监视数据，增加重载方法功能

---
+++ QxLibrary.c
@@ -0,0 +1,217 @@
+#include <xdef.h>
+void XI_CDECL widget_set_vint_value(xlong h, xint proid, xint value){
+	
+	
+	
+}
+
+xbool XI_CDECL widget_get_bool_value(xlong h, xint proid){
+	
+	
+	
+}
+
+XObject * XI_CDECL widget_get_object(xlong h, xint proid){
+	
+	
+	
+}
+
+xlong XI_CDECL locaUiFile(xstring uifile){
+	
+	
+	
+}
+
+void XI_CDECL ShowUi(xlong h){
+	
+	
+	
+}
+
+double XI_CDECL widget_get_double_value(xlong h, xint proid){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_double_value(xlong h, xint proid, double v){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_bool_value(xlong h, xint proid, xbool v){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_bkrl(xlong h, xint r){
+	
+	
+	
+}
+
+xint XI_CDECL widget_get_int_value(xlong h, xint proid){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_v2int_value(xlong h, xint proid, xint xv, xint yv){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_native_value(xlong h, xint proid, xlong value){
+	
+	
+	
+}
+
+void XI_CDECL widget_slot_string(xlong h, xint proid, xstring value){
+	
+	
+	
+}
+
+void XI_CDECL widget_slot(xlong h, xint proid){
+	
+	
+	
+}
+
+xlong XI_CDECL createQxApplication(XObject * obj){
+	
+	
+	
+}
+
+void XI_CDECL ApplicationRun(xlong g){
+	
+	
+	
+}
+
+xbool XI_CDECL core_sub_class(xlong h, XObject * pthis, XObject * subclass){
+	
+	
+	
+}
+
+XObject * XI_CDECL findControl(xlong h, xstring name){
+	
+	
+	
+}
+
+xstring XI_CDECL core_getName(xlong h){
+	
+	
+	
+}
+
+xstring XI_CDECL core_getClassName(xlong h){
+	
+	
+	
+}
+
+XObject * XI_CDECL core_getParent(xlong h){
+	
+	
+	
+}
+
+xstring XI_CDECL core_getString(xlong h, xint proid){
+	
+	
+	
+}
+
+xlong XI_CDECL createQObject(xint type, XObject * xobj, xlong parent){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_intstring_value(xlong h, xint proid, xint xv, xstring yv){
+	
+	
+	
+}
+
+xlong XI_CDECL createNObject(xint type, xlong param){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_v3int_value(xlong h, xint proid, xint xv, xint yv, xint zv){
+	
+	
+	
+}
+
+void XI_CDECL widget_set_intintstring_value(xlong h, xint proid, xint xv, xint yv, xstring zv){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_string(xlong h, xint proid, xstring name){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_handle_string(xlong h, xint proid, xlong handle, xstring name){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_string2(xlong h, xint proid, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_handle(xlong h, xint proid, xlong h1){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_handle2(xlong h, xint proid, xlong hv, xlong h1){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_string_handle_string2(xlong h, xint proid, xstring text, xlong handle, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_string_handle_string_int(xlong h, xint proid, xstring text, xlong handle, xstring v1, xint v2){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_string_handle_string2(xlong h, xint proid, xstring h1, xstring text, xlong handle, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XObject * XI_CDECL object_get_string_handle_string_int(xlong h, xint proid, xstring h1, xstring text, xlong handle, xstring v1, xint v2){
+	
+	
+	
+}
+
